fb434839bd40f4cc2668db0acfb0b7eaf36f65dd
BaST <bast33@yandex.ru>
2023-03-05 12:52:44 +0600

    context_processor for displaying cart

--- easyshop/easyshop/settings.py
+++ easyshop/easyshop/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
-    "django_browser_reload",
+    'django_browser_reload',
 
     'shop',
     'cart',
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
