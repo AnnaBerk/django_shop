2f2f979ecb1ea5f6d4e2c1798cd6a3b9633ddfd3
BaST <bast33@yandex.ru>
2023-03-05 18:31:40 +0600

    orders view and templates

--- easyshop/easyshop/urls.py
+++ easyshop/easyshop/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
